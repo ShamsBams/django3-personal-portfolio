5e68ce28e551fee90931d22f964c5735b3556b89
ShamsBams <shamsbams@gmail.com>
2022-07-31 12:13:08 +0000

    mod settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ygs8im9ij6@v34d%obbi^*$k+dirzb7$qf+aw&4@f5)t&uypak'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
